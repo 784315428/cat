b02fb19c7c68496838bd363ac7989f683a074311
784315428 <784315428@qq.com>
2019-05-19 20:00:17 +0800

    cat init

---
+++ target/cat/js/index2.js
@@ -0,0 +1,19 @@
+var socket = new WebSocket("ws://192.168.100.9:8080/cat/index");
+socket.onopen = function () {
+    console.log("WebSocket,建立连接成功")
+};
+socket.onclose = function (event) {
+    console.log("WebSocket,已关闭")
+};
+socket.onerror = function (event) {
+    console.log("WebSocket,异常")
+};
+socket.onmessage = socket_onmessage;
+socket["sendJson"] = function (param) {
+    if (socket.readyState===1) {
+        socket.send(JSON.stringify(param));
+    }else{
+        console.log("socketnotOk"+socket.readyState);
+    }
+
+};
